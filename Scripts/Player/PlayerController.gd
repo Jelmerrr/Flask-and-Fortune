d5d2d1a2e189d756631027836e077736794e2804
Jelmerrr <Jelljel@hotmail.com>
2026-06-07 13:26:29 +0200

    Some movement and input tweaks

--- Scripts/Player/PlayerController.gd
+++ Scripts/Player/PlayerController.gd
@@ -2,13 +2,15 @@ extends CharacterBody2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-var speed = 60  # speed in pixels/sec
+var speed = 80  # speed in pixels/sec
 var accel = 32
 
 
 func _physics_process(delta):
-	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = lerp(velocity, direction * speed, delta * accel)
-	if direction.x <= 0:
+	if velocity.x <= 0:
 		animated_sprite_2d.flip_h=direction.x
+	if direction.x != 0.0 && direction.y != 0.0:
+		position = round(position)
 	move_and_slide()
