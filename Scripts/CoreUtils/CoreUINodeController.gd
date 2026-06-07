a78acadf60bf3457922d4044680741e6fe811f55
Jelmerrr <Jelljel@hotmail.com>
2026-06-07 14:15:19 +0200

    Small code cleanups

--- Scripts/CoreUtils/CoreUINodeController.gd
+++ Scripts/CoreUtils/CoreUINodeController.gd
@@ -1,8 +1,7 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#Force control objects to be alligned to 480 x 270 scale
+	#Force control objects to be alligned to 480 x 270 scale 
+	#(mostly to prevent entire control modules misalligning with world scale)
 	for child in self.get_children():
 		child.position = child.position.snapped(Vector2(2,2))
--- Scripts/Player/PlayerController.gd
+++ Scripts/Player/PlayerController.gd
@@ -2,15 +2,18 @@ extends CharacterBody2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-var speed = 80  # speed in pixels/sec
-var accel = 32
-
+var speed = 80 #Speed in pixels/sec
+var accel = 32 #Acceleration in pixel/sec
 
 func _physics_process(delta):
+	#Get direction based on input and lerp it based on accel value
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = lerp(velocity, direction * speed, delta * accel)
+	#Flip sprite based on movement TODO: Change this to 4/8 directional when player art is properly done
 	if velocity.x <= 0:
 		animated_sprite_2d.flip_h=direction.x
+	#Round position if moving diagonal for smoother camera
 	if direction.x != 0.0 && direction.y != 0.0:
 		position = round(position)
+	#Adjust object position
 	move_and_slide()
