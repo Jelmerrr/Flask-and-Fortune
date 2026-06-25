d10e1014fd12d332c98c0f4fc8499d1f15af9f1b
Jelmerrr <Jelljel@hotmail.com>
2026-06-25 10:44:27 +0200

    House lighting

--- Scripts/Visual/Lighting/PointLightController.gd
+++ Scripts/Visual/Lighting/PointLightController.gd
@@ -1,6 +1,7 @@
 extends PointLight2D
 
 @export var isDeterminedByDayNightCycle: bool
+@export_range(0.0, 16.0) var maxEnergy: float = 1.0
 
 func _physics_process(delta: float) -> void:
-	if isDeterminedByDayNightCycle: self.energy = (-sin(GlobalTimeOfDayController.time - 0.5 * PI) + 1.0) * 0.5
+	if isDeterminedByDayNightCycle: self.energy = (-sin(GlobalTimeOfDayController.time - 0.5 * PI) + 1.0) * 0.5 * maxEnergy
